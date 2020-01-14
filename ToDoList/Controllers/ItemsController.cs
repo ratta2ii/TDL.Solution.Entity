a6930fa395d9cf7e36abae1cbf6e8ae8c081b5ab
tismetrm@gmail.com <example@example.com>
2020-01-14 14:46:49 -0800

    update todo list

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -33,5 +33,11 @@ namespace ToDoList.Controllers
         return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+{
+    Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    return View(thisItem);
+}
+
   }
 }
